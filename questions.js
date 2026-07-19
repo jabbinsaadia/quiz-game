6f0a0fed2540aa7a769159bb577c3d54d0d35369
jabbinsaadia <jabbinsaadia7@gmail.com>
2026-07-20 00:21:20 +0500

    options

--- questions.js
+++ questions.js
@@ -1,42 +1,59 @@
 let questions = [
     {
         numb: 1,
-        question: "",
-        answer: "",
-        option: [
+        question: "Which language is used to structure the content of a web page?",
+        answer: "B. HTML",
+        options: [
+            "A. CSS",
+            "B. HTML",
+            "C. JavaScript",
+            "D. Python"
 
         ]
     },
     {
         numb: 2,
-        question: "",
-        answer: "",
-        option: [
-            
+        question: "Which CSS property is used to add space inside an element's border?",
+        answer: "B. Padding",
+        options: [
+            "A. Margin",
+            "B. Padding",
+            "C. Border",
+            "D. Spacing"            
         ]
     },
     {
         numb: 3,
-        question: "",
-        answer: "",
-        option: [
+        question: "Which JavaScript function is used to display a popup message?",
+        answer: "C. alert()",
+        options: [
+            "A. print()",
+            "B. prompt()",
+            "C. alert()",
+            "D. console.log()"
             
         ]
     },
     {
         numb: 4,
-        question: "",
-        answer: "",
-        option: [
-            
+        question: "Which HTML tag is used to insert a line break?",
+        answer: "B. br",
+        options: [
+            "A. break",
+            "B. br",
+            "C. lb",
+            "D. hr"
         ]
     },
     {
         numb: 5,
-        question: "",
-        answer: "",
-        option: [
-            
+        question: "Which CSS property is used to make text bold?",
+        answer: "D. font-weight",
+        options: [
+            "A. font-style",
+            "B. text-decoration",
+            "C. text-transform",
+            "D. font-weight"
         ]
     },
 ];
