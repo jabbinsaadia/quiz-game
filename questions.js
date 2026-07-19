cb9657e810979911abacbc370e3094db385996bc
jabbinsaadia <jabbinsaadia7@gmail.com>
2026-07-19 19:06:51 +0500

    questions

---
+++ questions.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        numb: 1,
+        question: "",
+        answer: "",
+        option: [
+
+        ]
+    },
+    {
+        numb: 2,
+        question: "",
+        answer: "",
+        option: [
+            
+        ]
+    },
+    {
+        numb: 3,
+        question: "",
+        answer: "",
+        option: [
+            
+        ]
+    },
+    {
+        numb: 4,
+        question: "",
+        answer: "",
+        option: [
+            
+        ]
+    },
+    {
+        numb: 5,
+        question: "",
+        answer: "",
+        option: [
+            
+        ]
+    },
+];
